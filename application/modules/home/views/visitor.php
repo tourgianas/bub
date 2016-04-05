5a2003366f14628ffde500bcd4ec4a401f910538
tourgianas <tourgianas@gmail.com>
2016-04-05 18:52:39 +0300

    Logo sthn arxh

--- application/modules/home/views/visitor.php
+++ application/modules/home/views/visitor.php
@@ -56,21 +56,21 @@
      	<div class="slider">
 		    <ul class="slides">
 		      <li>
-		        <img src="<?php echo base_url("images/BUB imag.jpg"); ?>"> <!-- random image -->
+		        <img src="<?php echo base_url("images/BUB_imag.jpg"); ?>"> 
 		        <div class="caption center-align">
 		          <h3>This is our big Tagline!</h3>
 		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
 		        </div>
 		      </li>
 		      <li>
-		        <img src="<?php echo base_url("images/Mobiles.jpg"); ?>"> <!-- random image -->
+		        <img src="<?php echo base_url("images/Mobiles.jpg"); ?>"> 
 		        <div class="caption left-align">
 		          <h3>Left Aligned Caption</h3>
 		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
 		        </div>
 		      </li>
 		      <li>
-		        <img src="<?php echo base_url("images/fridge.jpg"); ?>"> <!-- random image -->
+		        <img src="<?php echo base_url("images/fridge.jpg"); ?>"> 
 		        <div class="caption right-align">
 		          <h3>Right Aligned Caption</h3>
 		          <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
