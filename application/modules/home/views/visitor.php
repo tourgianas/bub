a46c74e6df31075f789a6913b6113989677056ee
tourgianas <tourgianas@gmail.com>
2016-04-05 19:25:42 +0300

    SideBar

--- application/modules/home/views/visitor.php
+++ application/modules/home/views/visitor.php
@@ -24,11 +24,67 @@
 			<li>
 			<div class="collapsible-header"><i class="material-icons">stay_current_portrait</i>Κινητά Τηλέφωνα</div>
 			<div class="collapsible-body"><p>Εδώ θα βρείτε τα πάντα για τα κινητά.</p>
-				<ul class="collection">
-			        <li class="collection-item dismissable"><div>Samsung<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>Nokia<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>LG<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>Apple<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
+  				<ul class="collapsible" data-collapsible="expandable">
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Κατασκευαστής</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογες</label>
+					    </p>
+					    </form>
+			    </div></li>
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Μνήμη Ram</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογές</label>
+					    </p>
+					    </form>
+			    </div></li>
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Επεξεργαστής</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογές</label>
+					    </p>
+					    </form>
+			    </div></li>
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Μέγεθος Οθόνης</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογές</label>
+					    </p>
+					    </form>
+			    </div></li>
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Εσωτερική Μνήμη</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογές</label>
+					    </p>
+					    </form>
+			    </div></li>
+  				<li>
+				<div class="collapsible-header"><i class="material-icons"></i>Κάμερα</div>
+				<div class="collapsible-body">
+			        	<form action="#">
+					    <p>
+					      <input type="checkbox" id="test5" />
+					      <label for="test5">Επιλογές</label>
+					    </p>
+					    </form>
+			    </div></li>
 			    </ul>
 			</div>
 			</li>
@@ -36,10 +92,10 @@
 			<div class="collapsible-header"><i class="material-icons">view_list</i>Ψυγεία</div>
 			<div class="collapsible-body"><p>Όλα τα ψυγεία της αγοράς.</p>
 				<ul class="collection">
-			        <li class="collection-item dismissable"><div>Bosch<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>Siemens<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>Pitsos<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
-			        <li class="collection-item dismissable"><div>Whirlpool<a href="#!" class="secondary-content"><i class="material-icons">send</i></a></div></li>
+			        <li class="collection-item dismissable"><div>Bosch<a href="#!" class="secondary-content"><i class="material-icons"></i></a></div></li>
+			        <li class="collection-item dismissable"><div>Siemens<a href="#!" class="secondary-content"><i class="material-icons"></i></a></div></li>
+			        <li class="collection-item dismissable"><div>Pitsos<a href="#!" class="secondary-content"><i class="material-icons"></i></a></div></li>
+			        <li class="collection-item dismissable"><div>Whirlpool<a href="#!" class="secondary-content"><i class="material-icons"></i></a></div></li>
 			    </ul>
 			</div>
 			</li>
